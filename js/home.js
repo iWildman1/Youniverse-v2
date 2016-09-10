9937caf23e5c9370d3a0591b00f9465bc182aa67
Daniel Wildman <Dan@Daniels-iMac.local>
2016-09-10 13:50:12 +0100

    Implemented nav and fixed bar first draft

--- js/home.js
+++ js/home.js
@@ -1,3 +1,5 @@
+// Handle MouseEnter and MouseLeave for Work Items
+
 $('.work-item').mouseenter(function() {
   $(this).find('.work-item-info').removeClass('item-hidden');
 });
@@ -6,9 +8,61 @@ $('.work-item').mouseleave(function() {
   $(this).find('.work-item-info').addClass('item-hidden');
 });
 
+// Handle Masonry/Packery grid for Social Media (Homepage)
+
 var elem = document.querySelector('.social-media-grid');
 var pckry = new Packery(elem, {
   itemSelector: '.social-media-item',
   columnWidth: '.grid-sizer',
   percentageWidth: true
+})
+
+// Handle Navigation Toggle
+
+var navigationState = false;
+
+$('.nav-toggle').click(function() {
+  if (!navigationState) {
+    $('.navigation-main').css('display', 'block');
+
+    setTimeout(function() {
+      $('.navigation-main').removeClass('navigation-closed');
+    }, 25);
+
+    navigationState = true;
+  } else {
+    $('.navigation-main').addClass('navigation-closed');
+    navigationState = false;
+    setTimeout(function() {
+      $('.navigation-main').css('display', 'none');
+    }, 300);
+  }
+})
+
+//Handle Navigation Affix
+
+var bodyOffset = 0;
+var scrollPos = 0;
+var passedMarker = false;
+var navFixed = false;
+
+$(window).scroll(function() {
+  scrollPos = $(this).scrollTop();
+  bodyOffset = $('.main-body').offset().top;
+
+  if (scrollPos > (bodyOffset - 80)) {
+    passedMarker = true;
+  } else {
+    passedMarker = false;
+  }
+
+  console.log(passedMarker);
+
+  if (passedMarker && !navFixed) {
+    $('.navigation-bar').addClass('navigation-fixed');
+    navFixed = true;
+  } else if (!passedMarker && navFixed) {
+    $('.navigation-bar').removeClass('navigation-fixed');
+    navFixed = false;
+  }
 })
